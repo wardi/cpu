b172d2d0d868d338d08b30f66d008137bcf93490
Ian Ward <ian@excess.org>
2021-08-08 08:01:31 -0400

    car graphics

---
+++ car.py
@@ -0,0 +1,23 @@
+##### ##### ##### ## character cells are 5x8 with 1 pixel gap in between
+opponent = '''\
+    XXX   XXXXXX
+   XXX XXXXXXX  X
+  XXX XXXXXXX    XX
+ XXX XXXXXXX    XXXX
+XXXXXXXXXXXXXXXXXXXX
+ XXXXXXXXXXXXXXXXXX
+ X X  X XX X  X XX
+    XX      XX
+'''
+
+##### ##### ##### keep within 3 chars to reserve 1 byte of cgram
+player = '''\
+    XX  XXXXXX
+   X  XX      XX
+  X       XXXX  X
+ X  XXX  XXXX   X
+X              X
+ X XX    XX   X
+  XXXXXXXXXXXX
+   XX    XX
+'''
