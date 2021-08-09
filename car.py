2491f356862108a3a2807f01f0d2d2988f745458
Ian Ward <ian@excess.org>
2021-08-09 00:49:07 -0400

    break car into grid

--- car.py
+++ car.py
@@ -25,17 +25,38 @@ X              X
 '''
 
 def text_to_bin(t):
+    '''
+    text graphic -> list of integers, 1 per row, 1 bit per pixel
+    '''
     lines = t.strip('\n').split('\n')
     width = max(len(l) for l in lines)
+    o = []
     for l in lines:
-        p = 2**(width)
+        p = 2 ** (width - 1)
         n = 0
         for c in l:
             if c == 'X':
                 n |= p
             p >>= 1
+        o.append(n)
+    return o
 
-        print(bin(n).rjust(width+3))
+def bin_to_grid(b):
+    '''
+    break down binary graphic into 5x8 cells, 1 per horizontal bit shift
+    '''
+    o = []
+    shift = 1
+    while True:
+        g = [row << 5 >> shift for row in b]
+        if not any(row for row in g):
+            break
+        o.append([row & 0x1f for row in g])
+        shift += 1
+    return o
 
-text_to_bin(OPPONENT)
-text_to_bin(PLAYER)
+opponent = text_to_bin(OPPONENT)
+player = text_to_bin(PLAYER)
+
+print(bin_to_grid(opponent))
+print(bin_to_grid(player))
