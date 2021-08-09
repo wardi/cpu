ac0a23be7d0f06abc2bcbc4c20588bcafde320ca
Ian Ward <ian@excess.org>
2021-08-09 00:27:29 -0400

    cars to binary

--- car.py
+++ car.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+
 ##### ##### ##### ## character cells are 5x8 with 1 pixel gap in between
-opponent = '''\
+OPPONENT = '''
     XXX   XXXXXX
    XXX XXXXXXX  X
   XXX XXXXXXX    XX
@@ -11,7 +13,7 @@ XXXXXXXXXXXXXXXXXXXX
 '''
 
 ##### ##### ##### keep within 3 chars to reserve 1 byte of cgram
-player = '''\
+PLAYER = '''
     XX  XXXXXX
    X  XX      XX
   X       XXXX  X
@@ -21,3 +23,19 @@ X              X
   XXXXXXXXXXXX
    XX    XX
 '''
+
+def text_to_bin(t):
+    lines = t.strip('\n').split('\n')
+    width = max(len(l) for l in lines)
+    for l in lines:
+        p = 2**(width)
+        n = 0
+        for c in l:
+            if c == 'X':
+                n |= p
+            p >>= 1
+
+        print(bin(n).rjust(width+3))
+
+text_to_bin(OPPONENT)
+text_to_bin(PLAYER)
