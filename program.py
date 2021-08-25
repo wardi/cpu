b4bdfd48f2688f35d0e0302b267ba55e259ea7fe
Ian Ward <ian@excess.org>
2021-08-25 19:28:26 -0400

    sample program data for eeprom + lcd

---
+++ program.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+data = b''.join(
+    [
+        bytes(
+            [
+                0b10111000, # function set
+                0b10001110, # Display on, cursor
+                0b10000110, # Edit mode
+            ]
+        ),
+        open(
+            '/usr/share/dict/words',
+            'r',
+            encoding='utf-8',
+        ).read(32767).encode('ascii', 'replace'),
+    ]
+)
+
+sys.stdout.buffer.write(data)
