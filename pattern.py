aed7405fa050bf532fffd3faf431e1bf430aca4c
Ian Ward <ian@excess.org>
2022-01-06 21:27:59 -0500

    ducklings

--- pattern.py
+++ pattern.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 
 import math
+import random
+
+RAND = random.Random()
+
 
 def blink(n):
     yield [1] * n
     yield [0] * n
 
-def strobe(n):
+def sweep(n):
     for r in range(n):
         yield [int(i == r) for i in range(n)]
     yield [0] * n
 
-    for r in range(n):
-        yield [int(i == r) for i in reversed(range(n))]
-    yield [0] * n
-
-def sweep(n):
+def shadow(n):
     for r in range(n):
         yield [int(r <= i <= r*2) for i in range(n)]
-    yield [0] * n
-
-    for r in range(n):
-        yield [int(r <= i <= r*2) for i in reversed(range(n))]
     yield [0] * n
 
 def wave(n):
@@ -32,23 +28,40 @@ def wave(n):
         ]
     yield [0] * n
 
-    for r in range(1, n):
-        yield [
-            int(i < math.sin(math.pi * r / n) * n)
-            for i in reversed(range(n))
-        ]
+def ducklings(n, rand=RAND):
+    # initial positions
+    pos = list(range(0, int(-2 * n / 3), -3))
+    offset = list(pos)
+    target = 0.0
+    while any(p < n for p in pos):
+        yield [int(i in pos) for i in range(n)]
+
+        # advance desired position
+        target += 0.3
+        # wander towards desired position
+        for i, o in enumerate(offset):
+            move = pos[i] - target - o + rand.random() * 3
+            if move < 1:
+                pos[i] += 1
+            elif move >= 2:
+                pos[i] -= 1
     yield [0] * n
 
-LEDS = 8
+
+LEDS = 28
 PROGRAM = (
     blink,
     blink,
-    strobe,
     sweep,
+    lambda n: (reversed(x) for x in sweep(n)),
+    shadow,
+    lambda n: (reversed(x) for x in shadow(n)),
     wave,
+    lambda n: (reversed(x) for x in wave(n)),
+    ducklings,
 )
 
 if __name__ == '__main__':
     for fn in PROGRAM:
         for x in fn(LEDS):
-            print(x)
+            print(''.join('<>' if i else '..' for i in x))
