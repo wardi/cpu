a1873a9993420f826feb8a0f4fc5b1a265a82ecb
Ian Ward <ian@excess.org>
2022-03-01 21:42:17 -0500

    extract: remove unused code

--- bad-apple/extract.py
+++ bad-apple/extract.py
@@ -5,16 +5,6 @@ import gzip
 OUT_W = 5 * 8 + 7
 OUT_H = 8 * 4 + 3
 
-def draw_bits(bits):
-    print('\x1b[H', end='')
-    for y, row in enumerate(bits):
-        if y % 9 == 8:
-            print('.'* OUT_W)
-            continue
-        print(''.join(' X'[b] if x % 6 < 5 else '.' for x, b in enumerate(row)))
-    #import time
-    #time.sleep(1/30)
-
 video = cv2.VideoCapture('Bad Apple Edit3.mp4.mov')
 
 with gzip.open('badapple.encode-ready.gz', 'wb') as f:
@@ -25,7 +15,6 @@ with gzip.open('badapple.encode-ready.gz', 'wb') as f:
 
         frame = cv2.resize(frame, (OUT_W, OUT_H), interpolation=cv2.INTER_AREA)
         ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY_INV)
-#    cv2.imwrite(f'test{i:04d}.jpg', frame)
 
 # collapse [0, 0, 0] / [255, 255, 255] into 0 / 1
         bits = frame.reshape(OUT_H * OUT_W * 3)[::3].reshape(OUT_H, OUT_W) // 255
