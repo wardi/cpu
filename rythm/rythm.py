cfccc0b0c4dca880d5983ac83c53593b96ede4ae
Ian Ward <ian@excess.org>
2023-03-07 18:47:46 -0500

    clearer intro, add dissolve

--- rythm/rythm.py
+++ rythm/rythm.py
@@ -4,7 +4,7 @@ import sys
 import itertools
 
 import cmdconsts
-from cmdconsts import INI, HID, EIN, CLR, C00, D00, SL, SR
+from cmdconsts import *
 from cgram import CGDATA, CG, CGGRID, CGGRIDDATA, INTRO
 
 
@@ -31,6 +31,10 @@ def _scroll(delta):
     _scroll.pos = (_scroll.pos + delta) % SCROLL_WINDOW
 _scroll.pos = 0
 
+def clr():
+    out(CLR)
+    _scroll.pos = 0
+
 def up():
     '''scroll up (intro text)'''
     _scroll(1)
@@ -43,20 +47,21 @@ def top():
     '''return the 4 positions at the top of the screen'''
     a = _scroll.pos
     b = (a + WIDTH) % SCROLL_WINDOW
-    return f'E{a:02d}', f'D{a:02d}', f'E{b:02d}', f'D{b:02d}'
+    return f'E{b:02d}', f'D{b:02d}', f'E{a:02d}', f'D{a:02d}'
 
 def bottom():
     '''return the 4 positions at the bottom of the screen'''
-    a = (_scroll.pos - 1) % SCROLL_WINDOW
+    a = (_scroll.pos + WIDTH - 1) % SCROLL_WINDOW
     b = (a + WIDTH) % SCROLL_WINDOW
-    return f'E{a:02d}', f'D{a:02d}', f'E{b:02d}', f'D{b:02d}'
+    return f'E{b:02d}', f'D{b:02d}', f'E{a:02d}', f'D{a:02d}'
 
 # INIT
 out(INI)
 out(HID)
 out(EIN)
-out(CLR)
+clr()
 
+# INTRO
 out(C00)
 for mne in CGGRIDDATA:
     out(mnec(mne))
@@ -69,6 +74,30 @@ for itxt in INTRO:
         out(mnec(pos))
         out(b' ')
     up()
+
+# DISSOLVE (specific to CGGRID definition)
+dissolve = [
+    [C20, C30, C40, C50],
+    [C06, C16, C26, C56],
+    [C13, C23, C33],
+    [C21, C31],
+    [C41, C51],
+    [C07, C17],
+    [C27, C57],
+]
+def dis(d):
+    for di in d:
+        out(di)
+        out(B00)
+for d in dissolve[:-1]:
+    dis(d)
+    for pos in top():
+        out(mnec(pos))
+        out(b' ')
+    up()
+dis(dissolve[-1])
+clr()
+
 
 out(INI * (ROM_SIZE - out.written))
 sys.exit(0)
