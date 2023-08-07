6767f9e4c34a0b26fb932a081044fc1d0b4493f3
Ian Ward <ian@excess.org>
2023-08-07 18:17:45 -0400

    update maze generation

--- maze/genmaze.py
+++ maze/genmaze.py
@@ -3,73 +3,47 @@
 import sys
 import numpy as np
 from skimage.segmentation import flood_fill
+from skimage.io import imread
+from skimage.color import rgb2gray
 
 
-room = np.array([
-    [0, 1, 1, 0, 1, 1, 0],
-    [1, 2, 2, 2, 2, 2, 1],
-    [1, 2, 2, 2, 2, 2, 1],
-    [0, 2, 2, 1, 2, 2, 0],
-    [1, 2, 2, 2, 2, 2, 1],
-    [1, 2, 2, 2, 2, 2, 1],
-    [0, 1, 1, 0, 1, 1, 0],
-], dtype=np.uint8)
+# mask for cells above, below, left and right
+neighbours = np.array([
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]], dtype=np.uint8)
 
-lanes = np.array([
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 2, 1, 2, 1, 2, 1],
-    [0, 2, 1, 2, 1, 2, 0],
-    [1, 1, 1, 2, 1, 1, 1],
-    [0, 2, 1, 2, 1, 2, 0],
-    [1, 2, 1, 2, 1, 2, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-], dtype=np.uint8)
+def maze3(arr, gen):
+    if not gen:
+        gen = np.random.default_rng()
 
-spiral = np.array([
-    [0, 0, 2, 2, 2, 0, 0],
-    [1, 2, 2, 1, 1, 1, 0],
-    [1, 2, 1, 2, 2, 2, 1],
-    [1, 2, 1, 2, 1, 2, 1],
-    [1, 2, 2, 2, 1, 2, 1],
-    [0, 1, 1, 1, 2, 2, 1],
-    [0, 0, 2, 2, 2, 0, 0],
-], dtype=np.uint8)
+    unfilled = np.swapaxes(np.where(arr == 0), 0, 1)
+    gen.shuffle(unfilled)
 
-def box(width, height):
-    arr = np.zeros((height, width), dtype=np.uint8)
-    # walls
-    arr[0] = 1
-    arr[-1] = 1
-    arr[..., 0] = 1
-    arr[..., -1] = 1
-    start = (0, 1)
-    end = (-1, -2)
-    arr[start] = 2
-    arr[end] = 2
-    return arr
-
-
-def ant_nest(arr, generator=None):
-    if not generator:
-        generator = np.random.default_rng()
-
-    locs = np.swapaxes(np.where(arr == 0), 0, 1)
-    generator.shuffle(locs)
-
-    start = next(zip(*np.where(arr == 2)))
+    start = tuple(coord[0] for coord in np.where(arr == 2))
+    arr = np.copy(arr)
     arr[arr == 2] = 0
 
-    for lc in locs:
+    for lc in unfilled:
         lc = tuple(lc)
+
         y, x = lc
-        if sum(e == 0 for e in (arr[y-1,x], arr[y+1,x], arr[y,x-1], arr[y, x+1])) < 2:
+        # protect dead-ends from becoming walls
+        if np.sum(neighbours * arr[y-1:y+2, x-1:x+2]) > 2:
             continue
+
         arr[lc] = 1
         t = flood_fill(arr, start, 1, connectivity=1)
         if np.any(t == 0):
             arr[lc] = 0
 
     return arr
+
+
+def maze_from_template(fname, gen=None):
+    n = imread(fname)
+    n = np.array((n[...,0] == 188) + (n[...,0] == 252) * 2, dtype=np.uint8)
+    return maze3(n, gen)
 
 
 def braillepixels(a):
@@ -94,11 +68,11 @@ def braillepixels(a):
 
 
 if __name__ == '__main__':
-    mz = ant_nest(box(35,35), np.random.default_rng(42))
+    mz = maze_from_template(sys.argv[1], np.random.default_rng(int(sys.argv[2])))
     try:
-        output_name = sys.argv[1]
+        output_name = sys.argv[3]
     except IndexError:
         for l in braillepixels(mz):
             print(l)
     else:
-        np.save(output_name, mz, allow_pickle=False)
+        np.savetxt(output_name, mz, '%1d', '')
