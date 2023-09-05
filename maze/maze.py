a596ee81dc8b0a98a2429cb1676d36506aa46b42
Ian Ward <ian@excess.org>
2023-09-04 23:56:12 -0400

    player facing code

--- maze/maze.py
+++ maze/maze.py
@@ -81,10 +81,7 @@ def press_any_button(out, label, jmp):
 
 def init_cgram(out, label, jmp):
     out(CLR)
-    out(C00)
-    for b in cgram.PLAYER_VERTICAL_1:
-        out(opx(f'B{b:02d}'))
-
+    out(C10)
     for w in cgram.WALLS:
         for b in w:
             out(opx(f'B{b:02d}'))
@@ -98,48 +95,69 @@ def main_loop(out, label, jmp):
             if cell != '0':
                 continue
 
-            label(f'pos {pos_y},{pos_x}')
-            draw_map(pos_y, pos_x, out)
+            draw_map(pos_y, pos_x, out, label, jmp)
             label(f'input {pos_y},{pos_x}')
             if pos_y > TOP_Y and map_[pos_y - 1][pos_x] == '0':
                 out(HXU)
-                jmp(f'pos {pos_y - 1},{pos_x}')
+                jmp(f'pos {pos_y - 1},{pos_x} v')
             if map_[pos_y][pos_x + 1] == '0':
                 out(HXR)
-                jmp(f'pos {pos_y},{pos_x + 1}')
+                jmp(f'pos {pos_y},{pos_x + 1} r')
             if map_[pos_y + 1][pos_x] == '0':
                 out(HXD)
                 if pos_y < BOTTOM_Y:
-                    jmp(f'pos {pos_y + 1},{pos_x}')
+                    jmp(f'pos {pos_y + 1},{pos_x} v')
                 else:
                     jmp('intro')
             if map_[pos_y][pos_x - 1] == '0':
                 out(HXL)
-                jmp(f'pos {pos_y},{pos_x - 1}')
+                jmp(f'pos {pos_y},{pos_x - 1} l')
             jmp(f'input {pos_y},{pos_x}')
 
 
 
-def draw_map(pos_y, pos_x, out):
-    CPOS_TOP = [E20, D20, E00, D00]
+def draw_map(pos_y, pos_x, out, label, jmp):
+    CPOS_TOP = [D20, E20, E00, D00]
+    XDELTA = [-2, -5, 1, 4]
 
-    for x, cpos in zip(range(pos_x - 5, pos_x + 7, 3), CPOS_TOP):
+    def glyph(plr):
+        g = [
+            int(map_[pos_y][pos_x - 2]) * cgram.WALL_0[r]
+            + int(map_[pos_y][pos_x - 1]) * cgram.WALL_1[r]
+            + plr[r]
+            for r in range(8)
+        ]
+        out(C00)
+        for b in g:
+            out(opx(f'B{b:02d}'))
+
+    dirty = False
+    if map_[pos_y][pos_x - 1] == '0':
+        label(f'pos {pos_y},{pos_x} r')
+        glyph(cgram.PLAYER_RIGHT)
+        dirty = True
+
+    if map_[pos_y][pos_x + 1] == '0':
+        if dirty:
+            jmp(f'pos {pos_y},{pos_x}')
+        label(f'pos {pos_y},{pos_x} l')
+        glyph(cgram.PLAYER_LEFT)
+        dirty = True
+
+    if map_[pos_y - 1][pos_x] == '0' or map_[pos_y + 1][pos_x] == '0':
+        if dirty:
+            jmp(f'pos {pos_y},{pos_x}')
+        label(f'pos {pos_y},{pos_x} v')
+        glyph([
+            cgram.PLAYER_VERTICAL_1, cgram.PLAYER_VERTICAL_2
+            ][(pos_x + pos_y) % 2])
+
+    label(f'pos {pos_y},{pos_x}')
+    for xd, cpos in zip(XDELTA, CPOS_TOP):
+        x = pos_x + xd
         out(cpos)
         for y in range(pos_y - 9, pos_y + 11):
             if x + 2 == pos_x and y == pos_y:
-                plr = [
-                    cgram.PLAYER_VERTICAL_1,
-                    cgram.PLAYER_VERTICAL_2][(x + y) % 2]
-                glyph = [
-                    int(map_[y][x]) * cgram.WALL_0[r]
-                    + int(map_[y][x + 1]) * cgram.WALL_1[r]
-                    + plr[r]
-                    for r in range(8)
-                ]
-                out(C00)
-                for b in glyph:
-                    out(opx(f'B{b:02d}'))
-                out(D29)
                 out(CG0)
             else:
                 walls = (
