dcf318a164118b1537d66502de36dbfe034be170
Ian Ward <ian@excess.org>
2024-04-15 22:55:52 -0400

    simpler

--- opcodes/opcodes.py
+++ opcodes/opcodes.py
@@ -3,41 +3,39 @@
 import argparse
 from enum import IntEnum
 
-
-class Fl(IntEnum):
-    RS = 0b00000001  # Register Select  0: command, 1: data transfer
-    HC = 0b00000010  # Halt Clock  0: run, 1: halt
-
 LOOKUP_SIZE = 256
+
+RS = 1 << 0  # LCD Register Select  0: command, 1: data transfer
+HC = 1 << 1  # Halt Clock  0: run, 1: halt
 
 
 class Op(IntEnum):
-    HLT = 0b00000000  # Halt
-    CLR = 0b00000001  # Clear display
-    HOM = 0b00000010  # Home position
-    EDN = 0b00000100  # Cursor decrementing
-    EIN = 0b00000110  # Cursor incrementing
-    CUR = 0b00001110  # Display on, line cursor
-    INI = 0b00111011  # Initialize display
+    HLT = 0b0000_0000  # Halt
+    CLR = 0b0000_0001  # Clear display
+    HOM = 0b0000_0010  # Home position
+    EDN = 0b0000_0100  # Cursor decrementing
+    EIN = 0b0000_0110  # Cursor incrementing
+    CUR = 0b0000_1110  # Display on, line cursor
+    INI = 0b0011_1011  # Initialize display
     # Cursor positioning:
-    D20 = 0b10010100
-    D38 = 0b10100110
-    D39 = 0b10100111
-    E00 = 0b11000000
-    E19 = 0b11010011
-    E39 = 0b11100111
+    D20 = 0b1001_0100
+    D38 = 0b1010_0110
+    D39 = 0b1010_0111
+    E00 = 0b1100_0000
+    E19 = 0b1101_0011
+    E39 = 0b1110_0111
 
 
-p = argparse.ArgumentParser(description='Opcode example')
-p.add_argument( '--lookup', dest='lookup', type=argparse.FileType('wb'))
-p.add_argument( '--program', dest='program', type=argparse.FileType('wb'))
+p = argparse.ArgumentParser(description='Generate Opcode binaries')
+p.add_argument( '-l', dest='lookup', type=argparse.FileType('wb'))
+p.add_argument( '-p', dest='program', type=argparse.FileType('wb'))
 args = p.parse_args()
 
 if args.lookup:
-    table = bytearray([Fl.RS] * LOOKUP_SIZE)
+    table = bytearray([RS] * LOOKUP_SIZE)
     for op in Op:
         table[op.value] = 0
-    table[Op.HLT] = Fl.HC
+    table[Op.HLT] = HC
     args.lookup.write(table)
 
 if args.program:
